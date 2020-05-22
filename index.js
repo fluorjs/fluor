d40d0a96e5b2e590ff27f5750db962ee97b91aeb
madx <madx+github@yapok.org>
2020-05-22 23:37:43 +0200

    Revert "feat: Make event handling match nodes live"
    This reverts commit 835a100db9a88ace96836b70d53cf54be49767e7.

--- index.js
+++ index.js
@@ -218,7 +218,7 @@ function createMolecule(moleculeId, rootNode) {
 
   function classListMutation(mutation, className, selector = null) {
     return (ev) => {
-      const targets = selector ? $(selector, rootNode) : [ev.target]
+      const targets = selector ? $(selector, rootNode) : [ev.currentTarget]
       targets.forEach((target) => target.classList[mutation](className))
     }
   }
@@ -236,17 +236,14 @@ function createMolecule(moleculeId, rootNode) {
     },
 
     on(event, selector, fnOrArray) {
-      const handler = (ev) => {
-        if (
-          ev.target.matches(selector) &&
-          moleculeOf(ev.target).$root === rootNode
-        ) {
-          Array.isArray(fnOrArray)
-            ? fnOrArray.forEach((fn) => fn(ev))
-            : fnOrArray(ev)
+      const handler = Array.isArray(fnOrArray)
+        ? (ev) => fnOrArray.forEach((fn) => fn(ev))
+        : fnOrArray
+      for (const node of $(selector, rootNode)) {
+        if (moleculeOf(node).$root === rootNode) {
+          node.addEventListener(event, handler)
         }
       }
-      rootNode.addEventListener(event, handler)
     },
 
     addClass(className, selector = null) {
